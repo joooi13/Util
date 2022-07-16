99dd97d339d3b1d77708e1d437c096f2d6e7941c
joooi13 <joooi.y1313@gmail.com>
2022-07-16 17:38:10 +0900

    added google adsence

---
+++ public/js/ad.min.76751e86645f8aabba43fda028c2ac7fe4b7c0fd82a8ffaf5d59d4f37b12f52c.js
@@ -0,0 +1,1 @@
+var script=document.createElement("script");script.async=!0,script.setAttribute("src","https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3888041590832287"),script.setAttribute("crossorigin","anonymous"),document.getElementsByTagName("head")[0].appendChild(script)
