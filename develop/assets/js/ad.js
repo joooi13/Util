589975bde74c5b1dafb9af0daae45d4c85799a27
joooi13 <joooi.y1313@gmail.com>
2022-07-16 12:38:58 +0900

    added google adsence

--- develop/assets/js/ad.js
+++ develop/assets/js/ad.js
@@ -1,5 +1,1 @@
-<script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3888041590832287"
-  crossorigin="anonymous"
-></script>;
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3888041590832287" crossorigin="anonymous"></script>
